4b2ac58d53586dbc32611552de27ef6edf92849e
Jonathan R <jorenon@hotmail.fr>
2016-10-17 16:15:05 -0400

    pyqt5
    tests

--- workout_helper/main.py
+++ workout_helper/main.py
@@ -1,7 +1,15 @@
-
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
 
 def main():
-    print("Welcome to the Workout Helper!")
+    app = QApplication(sys.argv)
+    w = QWidget()
+    w.resize(250,150)
+    w.move(300,300)
+    w.setWindowTitle('Workout Helper')
+    w.show()
+
+    sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
